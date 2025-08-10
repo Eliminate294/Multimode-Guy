c94999a433afa0d02c0fcec330f3b439db516151
Nathanial Patrick <nathan@psfamily.co.uk>
2025-08-10 19:30:54 +0100

    add dev function to add roles to all verified

--- client.ts
+++ client.ts
@@ -21,6 +21,7 @@ interface ExtendedClient extends Client {
 const client: ExtendedClient = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
 	],
@@ -31,6 +32,7 @@ client.commands = new Collection();
 client.once(Events.ClientReady, async (readyClient) => {
 	await deployCommands();
 	console.log(`Discord bot logged in as ${readyClient.user.tag}`);
+	//await give_verified_all_roles("1403448698393854014");
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
@@ -51,5 +53,25 @@ client.on(Events.InteractionCreate, async (interaction) => {
 export const startBot = async () => {
 	await client.login(process.env.DISCORD_BOT_TOKEN);
 };
+
+async function give_verified_all_roles(role: string) {
+	console.log("running verified roles");
+	const guild = await client.guilds.cache.get("1371190654922657954");
+	console.log(guild);
+	const members = await guild!.members.fetch();
+	console.log(members);
+	for (const member of members.values()) {
+		if (member.roles.cache.has("1397401263485882448")) {
+			try {
+				await member.roles.add(role);
+				console.log(`Added role to ${member.user.tag}`);
+			} catch (err) {
+				console.error(`Couldn't add role to ${member.user.tag}:`, err);
+			}
+		} else {
+			console.log(`${member.user.tag} does not have verified role`);
+		}
+	}
+}
 
 export default client;
