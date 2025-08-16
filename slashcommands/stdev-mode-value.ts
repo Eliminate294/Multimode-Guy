f3bb3ed3937e5be1fe8e899bfb8481d1362e68eb
Nathanial Patrick <nathan@psfamily.co.uk>
2025-08-16 01:02:22 +0100

    add command

---
+++ slashcommands/stdev-mode-value.ts
@@ -0,0 +1,91 @@
+import {
+	ChatInputCommandInteraction,
+	GuildMember,
+	SlashCommandBuilder,
+} from "discord.js";
+import {
+	CompleteModeStats,
+	get_mode_stats,
+} from "../../func/psql/get_mode_stats.js";
+import { get_osu_discord } from "../../func/psql/get_osu_discord.js";
+import { calculate_stdev } from "../std_dev.js";
+import { EmbedBuilder } from "@discordjs/builders";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("stdev-mode-value")
+		.setDescription("Calculate how much PP you would need in a given mode ")
+		.addMentionableOption((option) =>
+			option
+				.setName("user")
+				.setDescription("Choose a user from the server")
+				.setRequired(false)
+		),
+
+	async execute(interaction: ChatInputCommandInteraction) {
+		const param = interaction.options.getMentionable("user", false);
+		const user: GuildMember = (
+			param ? param : interaction.user
+		) as GuildMember;
+		const tokenData = await get_osu_discord(user.id);
+		if (!tokenData) {
+			throw new Error(
+				`User '${user.displayName} | ${user.id}' not in database`
+			);
+		}
+		const allStats = await get_mode_stats(tokenData.user_id);
+		let pp: Map<string, number> = new Map();
+
+		for (const mode in allStats) {
+			pp.set(
+				mode,
+				Number(allStats[mode as keyof CompleteModeStats][0].pp)
+			);
+		}
+		const stdev = calculate_stdev(Array.from(pp.values()));
+		let calcStdev: Map<string, number> = new Map();
+		console.log(stdev);
+
+		console.log(pp);
+		for (const mode in allStats) {
+			const calcPP: Map<string, number> = new Map();
+			pp.forEach((pp, mode_key) => {
+				if (mode_key !== mode) {
+					calcPP.set(mode_key, pp);
+				} else {
+					calcPP.set(mode_key, pp + 1);
+				}
+			});
+			const modeStdev = await calculate_stdev(
+				Array.from(calcPP.values())
+			);
+			calcStdev.set(
+				mode,
+				Math.round(Math.abs(stdev - modeStdev) * 100) / 100
+			);
+			console.log(Math.abs(stdev - modeStdev));
+		}
+
+		const embed = new EmbedBuilder()
+			.setColor(0xffffff)
+			.setTitle("Standard Deviated Mode Value")
+			.addFields(
+				{
+					name: "Modes",
+					value: "Standard:\nTaiko:\nCTB:\nMania:",
+					inline: true,
+				},
+				{
+					name: "\u200B",
+					value: `${calcStdev.get("osu")} pp\n${calcStdev.get(
+						"taiko"
+					)} pp\n${calcStdev.get("fruits")} pp\n${calcStdev.get(
+						"mania"
+					)} pp`,
+					inline: true,
+				}
+			);
+
+		interaction.reply({ embeds: [embed] });
+	},
+};
