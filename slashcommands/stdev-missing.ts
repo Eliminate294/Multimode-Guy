a9f20dc7d9483f21697570591c4b8b2715a93942
Nathanial Patrick <nathan@psfamily.co.uk>
2025-09-02 13:21:23 +0100

    allow for osu user to be queried

--- slashcommands/stdev-missing.ts
+++ slashcommands/stdev-missing.ts
@@ -12,6 +12,7 @@ import {
 	get_mode_stats,
 } from "../../func/psql/get_mode_stats.js";
 import { calculate_missing } from "../std_dev.js";
+import { get_user_pp } from "../api/get_user.js";
 
 export default {
 	data: new SlashCommandBuilder()
@@ -45,31 +46,56 @@ export default {
 					{ name: "fruits", value: "fruits" },
 					{ name: "mania", value: "mania" }
 				)
+		)
+		.addStringOption((option) =>
+			option
+				.setName("username")
+				.setDescription("The username of the player")
+				.setRequired(false)
 		),
 
 	async execute(interaction: ChatInputCommandInteraction) {
-		const discord_id = interaction.user.id;
-		let db_info;
-		if (discord_id) {
-			db_info = await get_osu_discord(discord_id);
-			if (!db_info) {
-				console.log(
-					"User does not have a valid database entry:",
-					interaction.user.username
-				);
+		const user = interaction.options.getString("username", false);
+		let mode_pp;
+		if (user) {
+			mode_pp = await get_user_pp(interaction.user.id, user);
+			if (!mode_pp) {
 				await interaction.reply({
-					content:
-						"User could not be found, use /link to link your osu account to the bot",
+					content: "Failed to fetch data for specified user",
 					flags: MessageFlags.Ephemeral,
 				});
 				return;
 			}
 		} else {
-			console.log("No discord id");
-			return;
+			const discord_id = interaction.user.id;
+			let db_info;
+			if (discord_id) {
+				db_info = await get_osu_discord(discord_id);
+				if (!db_info) {
+					console.log(
+						"User does not have a valid database entry:",
+						interaction.user.username
+					);
+					await interaction.reply({
+						content:
+							"User could not be found, use /link to link your osu account to the bot",
+						flags: MessageFlags.Ephemeral,
+					});
+					return;
+				}
+				const mode_stats = await get_mode_stats(db_info.user_id, true);
+				mode_pp = {
+					osu: mode_stats.osu[0].pp,
+					taiko: mode_stats.taiko[0].pp,
+					fruits: mode_stats.fruits[0].pp,
+					mania: mode_stats.mania[0].pp,
+				};
+			} else {
+				console.log("No discord id");
+				return;
+			}
 		}
 
-		var db_pp: CompleteModeStats = await get_mode_stats(db_info.user_id);
 		const selected_mode = interaction.options.getString(
 			"mode",
 			true
@@ -78,9 +104,7 @@ export default {
 			["osu", "taiko", "fruits", "mania"] as const
 		).filter((mode) => mode !== selected_mode);
 
-		const pp: number[] = otherModes.map((mode) =>
-			Number(db_pp[mode][0].pp)
-		);
+		const pp: number[] = otherModes.map((mode) => Number(mode_pp[mode]));
 		const goal = interaction.options.getInteger("goal", true);
 		console.log(goal);
 
@@ -102,7 +126,7 @@ export default {
 			return;
 		}
 		console.log(needed);
-		const difference = Math.floor(needed) - db_pp[selected_mode][0].pp;
+		const difference = Math.floor(needed) - mode_pp[selected_mode];
 
 		const embed = new EmbedBuilder()
 			.setColor(0xffffff)
