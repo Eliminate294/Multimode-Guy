b4466283c365a431259fc43c6d3e47e4c95f2083
Nathanial Patrick <nathan@psfamily.co.uk>
2025-09-03 21:30:46 +0100

    remove debug text

--- slashcommands/stdev-missing.ts
+++ slashcommands/stdev-missing.ts
@@ -13,6 +13,8 @@ import {
 } from "../../func/psql/get_mode_stats.js";
 import { calculate_missing } from "../std_dev.js";
 import { get_user_pp } from "../api/get_user.js";
+import { get_osekai_data } from "../../func/psql/get_osekai_data.js";
+import { count } from "console";
 
 export default {
 	data: new SlashCommandBuilder()
@@ -46,7 +48,25 @@ export default {
 
 	async execute(interaction: ChatInputCommandInteraction) {
 		const user = interaction.options.getString("username", false);
+		const discord_id = interaction.user.id;
+		const db_info = await get_osu_discord(discord_id);
+
+		if (!db_info) {
+			console.log(
+				"User does not have a valid database entry:",
+				interaction.user.username
+			);
+			await interaction.reply({
+				content:
+					"You do not have a linked account with the bot, use /link to link your account",
+				flags: MessageFlags.Ephemeral,
+			});
+			return;
+		}
+
 		let mode_pp;
+		let invokerUser: Record<string, number | string> = {};
+		let username: string = user ? user : "N/A";
 		if (user) {
 			mode_pp = await get_user_pp(interaction.user.id, user);
 			if (!mode_pp) {
@@ -56,34 +76,32 @@ export default {
 				});
 				return;
 			}
+			const invoker_stats = await get_osekai_data(
+				undefined,
+				interaction.user.id
+			);
+			invokerUser = {
+				username: db_info.username,
+				country: db_info.country,
+				spp_rank: invoker_stats?.spp_rank ?? -1,
+				tpp_rank: invoker_stats?.tpp_rank ?? -1,
+			};
 		} else {
-			const discord_id = interaction.user.id;
-			let db_info;
-			if (discord_id) {
-				db_info = await get_osu_discord(discord_id);
-				if (!db_info) {
-					console.log(
-						"User does not have a valid database entry:",
-						interaction.user.username
-					);
-					await interaction.reply({
-						content:
-							"User could not be found, use /link to link your osu account to the bot",
-						flags: MessageFlags.Ephemeral,
-					});
-					return;
-				}
-				const mode_stats = await get_mode_stats(db_info.user_id, true);
-				mode_pp = {
-					osu: mode_stats.osu[0].pp,
-					taiko: mode_stats.taiko[0].pp,
-					fruits: mode_stats.fruits[0].pp,
-					mania: mode_stats.mania[0].pp,
-				};
-			} else {
-				console.log("No discord id");
-				return;
-			}
+			username = db_info.username;
+			const mode_stats = await get_mode_stats(db_info.user_id, true);
+			const invoker_stats = await get_osekai_data(db_info.user_id);
+			invokerUser = {
+				username: username,
+				country: db_info.country,
+				spp_rank: invoker_stats?.spp_rank ?? -1,
+				tpp_rank: invoker_stats?.tpp_rank ?? -1,
+			};
+			mode_pp = {
+				osu: mode_stats.osu[0].pp,
+				taiko: mode_stats.taiko[0].pp,
+				fruits: mode_stats.fruits[0].pp,
+				mania: mode_stats.mania[0].pp,
+			};
 		}
 
 		const goal = interaction.options.getInteger("goal", true);
@@ -118,8 +136,8 @@ export default {
 		}
 
 		const embed = {
-			title: "Calculate Missing PP",
-			description: `To reach **${goal}** spp, **Nathanial** would need:`,
+			//title: "Calculate Missing PP",
+			description: `To reach **${goal}** spp, **${username}** would need:`,
 			color: 8171961,
 			timestamp: new Date().toISOString(),
 			footer: {
@@ -130,37 +148,47 @@ export default {
 				url: "https://a.ppy.sh/9169747?1748281907.png",
 			},
 			author: {
-				name: `Nathanial | #1 spp | #1 tpp`,
-				url: "https://discordapp.com",
-				icon_url: "https://osu.ppy.sh/images/flags/GB.png",
+				name: `${invokerUser.username} | #${invokerUser.spp_rank} spp | #${invokerUser.tpp_rank} tpp`,
+				url: `https://osu.ppy.sh/users${db_info.user_id}`,
+				icon_url: `https://osu.ppy.sh/images/flags/${invokerUser.country}.png`,
 			},
 			fields: [
 				{
 					name: "<:osu:1405592882085367808>",
-					value: `\`${result.osu.needed}pp\` \`(${
+					value: `\`${result.osu.needed.toFixed(2)}pp\`\n\`(${
 						result.osu.difference >= 0 ? "+" : "-"
-					}${result.osu.difference}pp)\``,
+					}${result.osu.difference.toFixed(2)}pp)\``,
+					inline: true,
+				},
+				{
+					name: "\u200B",
+					value: "\u200B",
 					inline: true,
 				},
 				{
 					name: "<:taiko:1405592907733270629>",
-					value: `\`${result.taiko.needed}pp\` \`(${
+					value: `\`${result.taiko.needed.toFixed(2)}pp\`\n\`(${
 						result.taiko.difference >= 0 ? "+" : "-"
-					}${result.taiko.difference}pp)\``,
+					}${result.taiko.difference.toFixed(2)}pp)\``,
 					inline: true,
 				},
 				{
 					name: "<:catch:1405592919104294963>",
-					value: `\`${result.fruits.needed}pp\` \`(${
+					value: `\`${result.fruits.needed.toFixed(2)}pp\`\n\`(${
 						result.fruits.difference >= 0 ? "+" : "-"
-					}${result.fruits.difference}pp)\``,
+					}${result.fruits.difference.toFixed(2)}pp)\``,
+					inline: true,
+				},
+				{
+					name: "\u200B",
+					value: "\u200B",
 					inline: true,
 				},
 				{
 					name: "<:mania:1405592894630269069>",
-					value: `\`${result.mania.needed}pp\` \`(${
+					value: `\`${result.mania.needed.toFixed(2)}pp\`\n\`(${
 						result.mania.difference >= 0 ? "+" : "-"
-					}${result.mania.difference}pp)\``,
+					}${result.mania.difference.toFixed(2)}pp)\``,
 					inline: true,
 				},
 			],
