997eb988f00cb26f75e7262ab32bb31912a10581
Nathanial Patrick <nathan@psfamily.co.uk>
2025-09-04 05:26:42 +0100

    change emotes

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 export const MODES = ["osu", "taiko", "fruits", "mania"] as const;
 export const MODEEMOTES = {
-	osu: "<:osu:1405592882085367808>",
-	taiko: "<:taiko:1405592907733270629>",
-	fruits: "<:catch:1405592919104294963>",
-	mania: "<:mania:1405592894630269069>",
+	osu: "<:osu:1413017021104394300>",
+	taiko: "<:taiko:1413017052624715807>",
+	fruits: "<:catch:1413017071360675840>",
+	mania: "<:mania:1413017089257504901>",
 };
