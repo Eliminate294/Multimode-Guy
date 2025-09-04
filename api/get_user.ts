e488549eb4ab74e7ce81655f1484fc6953a027af
Nathanial Patrick <nathan@psfamily.co.uk>
2025-09-04 19:28:07 +0100

    update user object

--- api/get_user.ts
+++ api/get_user.ts
@@ -1,6 +1,9 @@
 import { get_users } from "../../func/api/get_users.js";
 import { stats } from "../../func/api/stats.js";
 import { get_token } from "../../func/psql/get_token.js";
+import { userObjects } from "../client.js";
+import { OSEKAI_STATS } from "../osekai.js";
+import { calculate_stdev } from "../std_dev.js";
 
 type Mode = "osu" | "taiko" | "fruits" | "mania";
 const modes: Mode[] = ["osu", "taiko", "fruits", "mania"];
@@ -30,6 +33,11 @@ export async function get_user_pp(
 		data.users[0].statistics_rulesets
 	) as Mode[]) {
 		modePP[mode] = data.users[0].statistics_rulesets[mode].pp;
+	}
+	const userObj = userObjects.get(invokerId);
+	if (userObj) {
+		userObj.tpp = Object.values(modePP).reduce((sum, val) => sum + val, 0);
+		userObj.spp = calculate_stdev(Object.values(modePP));
 	}
 	return modePP;
 }
