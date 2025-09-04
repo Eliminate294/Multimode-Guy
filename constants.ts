6a588fe58db269af1266e38ac23912277c879adb
Nathanial Patrick <nathan@psfamily.co.uk>
2025-09-04 04:57:02 +0100

    add mode emote constant

--- constants.ts
+++ constants.ts
@@ -1,1 +1,7 @@
 export const MODES = ["osu", "taiko", "fruits", "mania"] as const;
+export const MODEEMOTES = {
+	osu: "<:osu:1405592882085367808>",
+	taiko: "<:taiko:1405592907733270629>",
+	fruits: "<:catch:1405592919104294963>",
+	mania: "<:mania:1405592894630269069>",
+};
